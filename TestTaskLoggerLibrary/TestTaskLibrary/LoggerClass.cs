4aa70b8cccb8cee514ab85a38520a91e7ee7830f
Владимир Хранитель <Vovasaeastralnyj@gmail.com>
2024-01-15 16:08:11 +0300

    Vladimir v0.1 - wrote first iteration of method of checking Size.

--- TestTaskLoggerLibrary/TestTaskLibrary/LoggerClass.cs
+++ TestTaskLoggerLibrary/TestTaskLibrary/LoggerClass.cs
@@ -4,11 +4,54 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using NLog;
 
 namespace TestTaskLibrary
 {
     public class LoggerClass
     {
+        public int FileSize { get; set; }
+        public Guid UserID { get; set; }
+        public Guid Module {  get; set; }
+        public string ActionType { get; set; }
+        public string PathToSaveFile { get; set; }
 
+        public LoggerClass(int MaxFileSize, Guid UserID, Guid Module, string ActionType, string PathToSaveFile) 
+        {
+            this.FileSize = MaxFileSize;
+            this.UserID = UserID;
+            this.Module = Module;
+            this.ActionType = ActionType;
+            this.PathToSaveFile = PathToSaveFile;
+        }
+
+        public bool SizeCheck(string PathToFile)
+        {
+          FileInfo LogFileInfo = new FileInfo(PathToFile);
+          long FileSize = LogFileInfo.Length;
+
+          bool Result;
+          
+          if (FileSize > 500000000)
+          {
+            Result = true;
+          }
+          else
+          {
+            Result= false;
+          }
+          
+          return Result;
+        }
+
+        public void CreateNewFile()
+        {
+
+        }
+
+        public void CreateNewDirectory()
+        {
+
+        }
     }
 }
