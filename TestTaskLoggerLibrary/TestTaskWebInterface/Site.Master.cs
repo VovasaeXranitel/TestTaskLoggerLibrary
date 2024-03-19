cb9fc8bb99e772efd50c9d6dc2a6a5da3ae06316
Владимир Хранитель <Vovasaeastralnyj@gmail.com>
2024-03-19 20:46:59 +0300

    Vladimir/Development - ver 1 final coomit!

--- TestTaskLoggerLibrary/TestTaskWebInterface/Site.Master.cs
+++ TestTaskLoggerLibrary/TestTaskWebInterface/Site.Master.cs
@@ -1,12 +1,6 @@
 ﻿using Microsoft.Ajax.Utilities;
 using System;
-using System.Collections.Generic;
-using System.EnterpriseServices.CompensatingResourceManager;
-using System.IO;
-using System.Linq;
-using System.Web;
 using System.Web.UI;
-using System.Web.UI.WebControls;
 using TestTaskLibrary;
 
 namespace TestTaskWebInterface
@@ -18,11 +12,16 @@ namespace TestTaskWebInterface
 
         }
 
+        /// <summary>
+        /// Clicking a button that displays the text of the log file
+        /// </summary>
         protected void FileSearchButton_Click(object sender, EventArgs e)
         {
+            //Get the name of the module and file
             string moduleName = ModuleNameInsert.Text;
             string fileName = FileNameInsert.Text;
 
+            //We check whether the input is correct and display entries from the logs on the screen
             if (!moduleName.IsNullOrWhiteSpace() && !fileName.IsNullOrWhiteSpace())
             { 
                 string[] LogsData = LoggerClass.LogRead(moduleName, fileName);
@@ -32,6 +31,7 @@ namespace TestTaskWebInterface
                     LogLinesOutput.Items.Add(log);
                 }
             }
+            //If the data is not entered, an error message is displayed on the screen.
             else
             {
                 string errorLine = "An error has occurred, check that the entered module name and file name are correct";
